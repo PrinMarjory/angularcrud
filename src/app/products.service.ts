73d882d1f27e11e9d554f637098c4b1ba23df746
Marjory PRIN <marjory.prin@gmail.com>
2024-09-06 10:31:44 +0200

    Création du service de chargement des données

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -3,19 +3,27 @@ import {
   NewProductInterface,
   ProductInterface,
 } from './interfaces/product.interface';
+
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, catchError, throwError } from 'rxjs';
-
+ 
 @Injectable({
   providedIn: 'root',
 })
 
 export class ProductsService {
+
   static url = 'http://localhost:3000/products';
+  
   constructor(private http: HttpClient) {}
 
   addProduct(product: NewProductInterface): Observable<ProductInterface> {
     console.log(`Dans addProduct de ProductsService`, product);
     return this.http.post<ProductInterface>(ProductsService.url, product);
+  }
+
+  loadProducts(): Observable<ProductInterface[]> {
+    console.log(`Dans loadProduct de ProductsService`);
+    return this.http.get<ProductInterface[]>(ProductsService.url);
   }
 }
